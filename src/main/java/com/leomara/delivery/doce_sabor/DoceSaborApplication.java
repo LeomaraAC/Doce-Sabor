fd7c1f6fe4311a38f2e1b737b41da8aca0371d9f
LeomaraAC <leomara.andrade@gmail.com>
2019-10-31 18:04:28 -0300

    Base do projeto

---
+++ src/main/java/com/leomara/delivery/doce_sabor/DoceSaborApplication.java
@@ -0,0 +1,13 @@
+package com.leomara.delivery.doce_sabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoceSaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoceSaborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leomara/delivery/doce_sabor/DoceSaborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leomara.delivery.doce_sabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoceSaborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
