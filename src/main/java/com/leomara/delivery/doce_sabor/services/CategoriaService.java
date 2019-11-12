af72ec474a5762db9e10ab3bf27f7271c74a8558
LeomaraAC <leomara.andrade@gmail.com>
2019-11-11 22:44:04 -0300

    Criando os testes e a remoção de uma categoria.

--- src/main/java/com/leomara/delivery/doce_sabor/services/CategoriaService.java
+++ src/main/java/com/leomara/delivery/doce_sabor/services/CategoriaService.java
@@ -14,9 +14,21 @@ public class CategoriaService {
     CategoriaRepository repo;
 
     public void insert(Categoria categoria) {
+        categoria.setId(null);
         Optional<Categoria> obj = repo.findByNome(categoria.getNome());
         if (obj.isPresent())
             throw new CategoriaException("A categoria " + categoria.getNome() + " já esta cadastrada.");
         repo.save(categoria);
+    }
+
+    public void delete(int catId) {
+        Optional<Categoria> obj = repo.findById(catId);
+        if (!obj.isPresent())
+            throw new CategoriaException("Categoria não encontrada. ID: " + catId);
+        Categoria cat = obj.get();
+        if (cat.getProdutos().size() > 0)
+            throw new CategoriaException("Não é possível excluir uma categoria que possui produtos.");
+        repo.deleteById(catId);
+
     }
 }
--- src/test/java/com/leomara/delivery/doce_sabor/services/CategoriaServiceTest.java
+++ src/test/java/com/leomara/delivery/doce_sabor/services/CategoriaServiceTest.java
@@ -1,6 +1,7 @@
 package com.leomara.delivery.doce_sabor.services;
 
 import com.leomara.delivery.doce_sabor.domain.Categoria;
+import com.leomara.delivery.doce_sabor.domain.Produto;
 import com.leomara.delivery.doce_sabor.repositories.CategoriaRepository;
 import com.leomara.delivery.doce_sabor.services.exception.CategoriaException;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -34,15 +36,29 @@ public class CategoriaServiceTest {
     private Exception exception;
 
     private Categoria categoria;
+    private Produto p1;
+    private Produto p2;
 
-    private static final String NOME_CAT = "Categoria 1";
+    private static final String CAT_NOME = "Categoria 1";
     private static final int CAT_ID = 1;
+    private static final String PRODUTO_1 = "Produto 1";
+    private static final String PRODUTO_2 = "Produto 2";
 
 
     @BeforeEach
     public void setUp() {
-        categoria = new Categoria(CAT_ID, NOME_CAT);
+        categoria = new Categoria(CAT_ID, CAT_NOME);
+
+        p1 = new Produto();
+        p1.setNome(PRODUTO_1);
+
+        p2 = new Produto();
+        p2.setNome(PRODUTO_2);
+
+        when(repository.findById(CAT_ID)).thenReturn(Optional.of(categoria));
     }
+
+    /** Método Insert*/
 
     @Test
     public void deve_chamar_metodo_save_do_repositorio_ao_inserir() {
@@ -52,8 +68,32 @@ public class CategoriaServiceTest {
 
     @Test
     public void nao_deve_salvar_duas_categorias_com_o_mesmo_nome() {
-        when(repository.findByNome(NOME_CAT)).thenReturn(Optional.of(categoria));
+        when(repository.findByNome(CAT_NOME)).thenReturn(Optional.of(categoria));
         exception = assertThrows(CategoriaException.class, () -> sut.insert(categoria));
-        assertEquals("A categoria " + NOME_CAT + " já esta cadastrada.", exception.getMessage());
+        assertEquals("A categoria " + CAT_NOME + " já esta cadastrada.", exception.getMessage());
     }
+
+    /** Método Delete*/
+
+    @Test
+    public void nao_deve_deletar_categoria_com_id_inexistente() {
+        when(repository.findById(CAT_ID)).thenReturn(Optional.empty());
+        exception = assertThrows(CategoriaException.class, () -> sut.delete(CAT_ID));
+        assertEquals("Categoria não encontrada. ID: " + CAT_ID, exception.getMessage());
+    }
+
+    @Test
+    public void deve_chamar_metodo_delete_by_id_do_repositorio() {
+        sut.delete(CAT_ID);
+
+        verify(repository).deleteById(CAT_ID);
+    }
+
+    @Test
+    public void nao_deve_excluir_categoria_com_produtos_associados() {
+        categoria.setProdutos(Arrays.asList(p1, p2));
+        exception = assertThrows(CategoriaException.class, () -> sut.delete(CAT_ID));
+        assertEquals("Não é possível excluir uma categoria que possui produtos.", exception.getMessage());
+    }
+
 }
