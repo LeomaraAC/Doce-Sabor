f40809a5f3dc20e7a051c866ca633dafb25f0fbe
LeomaraAC <leomara.andrade@gmail.com>
2019-12-31 09:08:58 -0300

    Documentação dos endpoint.

--- src/main/java/com/leomara/delivery/doce_sabor/resources/EmpresaResource.java
+++ src/main/java/com/leomara/delivery/doce_sabor/resources/EmpresaResource.java
@@ -2,6 +2,9 @@ package com.leomara.delivery.doce_sabor.resources;
 
 import com.leomara.delivery.doce_sabor.domain.Empresa;
 import com.leomara.delivery.doce_sabor.services.EmpresaService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,11 @@ public class EmpresaResource {
     @Autowired
     EmpresaService service;
 
+    @ApiOperation(value = "Cadastra uma empresa")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Empresa cadastrada"),
+            @ApiResponse(code = 400, message = "Erro de validação")
+    })
     @PostMapping(produces = "application/json", consumes = "application/json")
     public ResponseEntity<Empresa> insert(@Valid @RequestBody Empresa empresa) {
         empresa = service.insert(empresa);
@@ -23,6 +31,11 @@ public class EmpresaResource {
         return ResponseEntity.created(uri).body(empresa);
     }
 
+    @ApiOperation(value = "Exclui uma empresa")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Empresa excluida"),
+            @ApiResponse(code = 404, message = "Empresa não encontrada")
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
