42e24d24e0713650a2b26d662009c2f8e01f594f
LeomaraAC <leomara.andrade@gmail.com>
2020-01-12 09:49:55 -0300

    Testes do endpoint de atualização de empresas.

--- src/test/java/com/leomara/delivery/doce_sabor/resources/EmpresaResourceTest.java
+++ src/test/java/com/leomara/delivery/doce_sabor/resources/EmpresaResourceTest.java
@@ -162,7 +162,6 @@ public class EmpresaResourceTest extends ConfigurationResourceTests {
                         "errors.message", hasItems(ERRO_PREENCHIMENTO_OBRIGATORIO));
     }
 
-    /**
     /** Deletar */
     @Test
     public void deve_retornar_excessao_ao_tenta_apagar_empresa_com_id_inexistente() {
@@ -187,6 +186,152 @@ public class EmpresaResourceTest extends ConfigurationResourceTests {
                 .statusCode(HttpStatus.NO_CONTENT.value());
     }
 
+    /** Ataualizar */
+    @Test
+    public void deve_retornar_excessao_ao_tentar_atualizar_empresa_com_id_inexistente() {
+        empresa.setId(ID_INEXISTENTE);
 
+        given()
+                .request()
+                .contentType(ContentType.JSON)
+                .pathParam("id", ID_INEXISTENTE)
+                .body(gson.toJson(empresa))
+        .when()
+                .put(URN_COM_ID)
+        .then()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .body("status", equalTo(HttpStatus.NOT_FOUND.value()),
+                        "message", equalTo(MSG_ERRO_EMPRESA_NAO_ENCONTRADA));
+    }
+
+    @Test
+    public void deve_retornar_excessao_ao_tentar_atualizar_empresa_com_CNPJ_ja_existente() {
+        empresa.setId(ID_EXISTENTE);
+        empresa.setCnpj(CNPJ_EXISTENTE);
+
+        given()
+                .request()
+                .contentType(ContentType.JSON)
+                .pathParam("id", ID_EXISTENTE)
+                .body(gson.toJson(empresa))
+        .when()
+                .put(URN_COM_ID)
+        .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("status", equalTo(HttpStatus.BAD_REQUEST.value()),
+                        "message", equalTo(MSG_ERRO_DUPLICIDADE_CNPJ));
+    }
+
+    @Test
+    public void deve_retornar_excessao_ao_tentar_atualizar_empresa_com_CNPJ_invalido() {
+        empresa.setId(ID_EXISTENTE);
+        empresa.setCnpj(CNPJ_INVALIDO);
+
+        given()
+                .request()
+                .contentType(ContentType.JSON)
+                .pathParam("id", ID_EXISTENTE)
+                .body(gson.toJson(empresa))
+        .when()
+                .put(URN_COM_ID)
+        .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("status", equalTo(HttpStatus.BAD_REQUEST.value()),
+                        "message", equalTo(ERRO_VALIDACAO_DADOS),
+                        "errors.field", containsInAnyOrder("cnpj"),
+                        "errors.message", containsInAnyOrder(MSG_ERRO_CNPJ_INVALIDO));
+    }
+
+    @Test
+    public void deve_retornar_excessao_ao_tentar_atualizar_empresa_com_email_ja_existente() {
+        empresa.setId(ID_EXISTENTE);
+        empresa.setEmail(EMAIL_EXISTENTE);
+
+        given()
+                .request()
+                .contentType(ContentType.JSON)
+                .pathParam("id", ID_EXISTENTE)
+                .body(gson.toJson(empresa))
+        .when()
+                .put(URN_COM_ID)
+        .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("status", equalTo(HttpStatus.BAD_REQUEST.value()),
+                        "message", equalTo(MSG_ERRO_DUPLICIDADE_EMAIL));
+    }
+
+    @Test
+    public void deve_retornar_excessao_ao_tentar_atualizar_empresa_com_email_invalido() {
+        empresa.setId(ID_EXISTENTE);
+        empresa.setEmail(EMAIL_INVALIDO);
+
+        given()
+                .request()
+                .contentType(ContentType.JSON)
+                .pathParam("id", ID_EXISTENTE)
+                .body(gson.toJson(empresa))
+        .when()
+                .put(URN_COM_ID)
+        .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("status", equalTo(HttpStatus.BAD_REQUEST.value()),
+                        "message", equalTo(ERRO_VALIDACAO_DADOS),
+                        "errors.field", containsInAnyOrder("email"),
+                        "errors.message", containsInAnyOrder(MSG_ERRO_EMAIL_INVALIDO));
+    }
+
+    @Test
+    public void deve_retornar_excessao_ao_tentar_atualizar_empresa_com_nome_fantasia_ja_existente() {
+        empresa.setId(ID_EXISTENTE);
+        empresa.setNome_fantasia(NOME_FANTASIA_EXISTENTE);
+
+        given()
+                .request()
+                .contentType(ContentType.JSON)
+                .pathParam("id", ID_EXISTENTE)
+                .body(gson.toJson(empresa))
+        .when()
+                .put(URN_COM_ID)
+        .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("status", equalTo(HttpStatus.BAD_REQUEST.value()),
+                        "message", equalTo(MSG_ERRO_DUPLICIDADE_NOME_FANTASIA));
+    }
+
+    @Test
+    public void deve_atualizar_empresa_de_id_3_ao_inves_do_id_4() {
+        empresa.setId(ID_NOVO); // 4
+
+        given()
+                .request()
+                .contentType(ContentType.JSON)
+                .pathParam("id", ID_EXISTENTE) // 3
+                .body(gson.toJson(empresa))
+        .when()
+                .put(URN_COM_ID)
+        .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("id", equalTo(ID_EXISTENTE),
+                        "nome_fantasia", equalTo(NOME_FANTASIA));
+    }
+
+    @Test
+    public void deve_atualizar_empresa_com_sucesso() {
+        empresa.setId(ID_EXISTENTE);
+
+        given()
+                .request()
+                .contentType(ContentType.JSON)
+                .pathParam("id", ID_EXISTENTE)
+                .body(gson.toJson(empresa))
+        .when()
+                .put(URN_COM_ID)
+        .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("id", equalTo(ID_EXISTENTE),
+                        "nome_fantasia", equalTo(NOME_FANTASIA),
+                        "cnpj", equalTo(CNPJ),
+                        "email", equalTo(EMAIL));
+    }
 }
 
